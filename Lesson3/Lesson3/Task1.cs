ab90f32017cf490b098d026aeacfd63a57880ba3
Pavel Shemelin <pauls0@mail.ru>
2018-11-12 12:25:07 +0300

    реализация Task1 полная

--- Lesson3/Lesson3/Task1.cs
+++ Lesson3/Lesson3/Task1.cs
@@ -16,24 +16,24 @@ namespace Lesson3
 
     partial class Lesson3
     {
-        
-        struct Complex
+        //вводим имя типа т.к. в проекте есть одноименный класс
+        struct StruComplex
         {
             #region Исходный код из методички
             public double im;
             public double re;
             //  в C# в структурах могут храниться также действия над данными
-            public Complex Plus(Complex x)
+            public StruComplex Plus(StruComplex x)
             {
-                Complex y;
+                StruComplex y;
                 y.im = im + x.im;
                 y.re = re + x.re;
                 return y;
             }
             //  Пример произведения двух комплексных чисел
-            public Complex Multi(Complex x)
+            public StruComplex Multi(StruComplex x)
             {
-                Complex y;
+                StruComplex y;
                 y.im = re * x.im + im * x.re;
                 y.re = re * x.re - im * x.im;
                 return y;
@@ -45,14 +45,78 @@ namespace Lesson3
             #endregion Исходный код из методички
 
             #region Реализация метода вычитания комплесных чисел
-            public Complex Minus(Complex x)
+            public StruComplex Minus(StruComplex x)
             {
-                Complex y;
+
+                StruComplex y;
+
                 y.im = im - x.im;
                 y.re = re - x.re;
                 return y;
+
             }
             #endregion Реализация метода вычитания комплесных чисел
+
+        }
+
+        class ClassComplex
+        {
+
+            #region Исходный код из методички
+            // Все методы и поля публичные. Мы можем получить доступ к ним из другого класса.
+            public double im;
+            public double re;
+
+            public ClassComplex Plus(ClassComplex x2)
+            {
+
+                ClassComplex x3 = new ClassComplex();
+
+                x3.im = this.im + x2.im;
+                x3.re = this.re + x2.re;
+
+                return x3;
+
+            }
+
+            public override string ToString()
+            {
+                return re + "+" + im + "i";
+            }
+            #endregion Исходный код из методички
+
+            #region Реализация метода вычитания и произведения комплесных чисел
+
+            public ClassComplex Minus(ClassComplex x2)
+            {
+
+                ClassComplex x3 = new ClassComplex();
+
+                x3.im = this.im - x2.im;
+                x3.re = this.re - x2.re;
+
+                return x3;
+
+            }
+
+            public ClassComplex Multu(ClassComplex x2)
+            {
+
+                ClassComplex x3 = new ClassComplex();
+
+                /*
+                y.im = re * x.im + im * x.re;
+                y.re = re * x.re - im * x.im;
+                */
+
+                x3.im = this.re * x2.im + this.im * x2.re;
+                x3.re = this.re * x2.re - this.im * x2.im;
+
+                return x3;
+
+            }
+
+            #endregion Реализация метода вычитания и произведения комплесных чисел
 
         }
 
@@ -62,25 +126,79 @@ namespace Lesson3
             string Welcome = "Вы выбрали задачу работы с комплексными числами\n";
             Welcome = Welcome + "Это так увлекательно!\n";
             Welcome = Welcome + "Давайте начнем.\n";
+            Welcome = Welcome + "1 - Сложение комплесных чисел\n";
+            Welcome = Welcome + "2 - Вычитание комплесных чисел\n";
+            Welcome = Welcome + "3 - Умножение комплесных чисел\n";
+            Welcome = Welcome + "0 - Выход в предыдущее меню\n";
 
             ClassLibraryLesson3.PrintTaskWelcomeScreen(Welcome);
 
-            Complex complexA = new Complex();
+            #region работа структуры 
+            /*
+            StruComplex complexA = new StruComplex();
             complexA.im = 2;
             complexA.re = 2;
 
-            Complex complexB = new Complex();
+            StruComplex complexB = new StruComplex();
             complexB.im = 5;
             complexB.re = 7;
 
-            Complex complexB_minus_A = complexB.Minus(complexA);
+            ClassLibraryLesson3.Print($"Операция: комп. число: {complexB.ToString()} - комп. число: {complexA.ToString()} = {complexB.Minus(complexA)}");
+            */
+            #endregion работа структуры 
 
-            ClassLibraryLesson3.Print($"Операция: комп. число: {complexB.ToString()} - комп. число: {complexA.ToString()} = {complexB_minus_A.ToString()}");
+            bool menuExit = false;
 
-            ClassLibraryLesson3.Pause();
+            //инициализация переменных
+            ClassComplex complexA = new ClassComplex();
+            complexA.im = 2;
+            complexA.re = 3;
+
+            ClassComplex complexB = new ClassComplex();
+            complexB.im = 5;
+            complexB.re = 7;
+
+            while (!(menuExit))
+            {
+
+                ClassLibraryLesson3.PrintTaskWelcomeScreen(Welcome);
+
+                ConsoleKeyInfo userChooseKey = Console.ReadKey(true);
+
+                switch (userChooseKey.Key)
+                {
+
+                    case ConsoleKey.D1:
+                    case ConsoleKey.NumPad1:
+
+                        ClassLibraryLesson3.Print($"Операция: комп. число: {complexA.ToString()} + комп. число: {complexB.ToString()} = {complexA.Plus(complexB)}");
+                        break;
+
+                    case ConsoleKey.D2:
+                    case ConsoleKey.NumPad2:
+
+                        ClassLibraryLesson3.Print($"Операция: комп. число: {complexA.ToString()} - комп. число: {complexB.ToString()} = {complexA.Minus(complexB)}");
+                        break;
+
+                    case ConsoleKey.D3:
+                    case ConsoleKey.NumPad3:
+
+                        ClassLibraryLesson3.Print($"Операция: комп. число: {complexA.ToString()} * комп. число: {complexB.ToString()} = {complexA.Multu(complexB)}");
+                        break;
+
+                    case ConsoleKey.D0:
+                    case ConsoleKey.NumPad0:
+                        menuExit = true; // на выход
+                        break;
+
+                }
+
+                if (!(menuExit)) ClassLibraryLesson3.Pause("Нажмите люубю клавишу.");
+
+            }
 
         }
-
+                    
     }
 
 }
